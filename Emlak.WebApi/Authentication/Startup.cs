0a933a227ead788a4b1d997c8c671b28bee26c6d
Çağlar Can SARIKAYA <63093864+caglarsarikaya@users.noreply.github.com>
2021-04-13 23:23:22 +0300

    first

---
+++ Emlak.WebApi/Authentication/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+
+[assembly: OwinStartup(typeof(Emlak.WebApi.Authentication.Startup))]
+
+namespace Emlak.WebApi.Authentication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            var config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+            WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            var oAuthAuthorizationServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(1),
+                AllowInsecureHttp = true,
+                Provider = new Saglayici()
+            };
+            app.UseOAuthAuthorizationServer(oAuthAuthorizationServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# real_agency_homework
+
+1- click restore nuget packages
+2-select Emlak.DAL to Default Project on Nuget Package Manager Console if there is occur an error you can try rebuilt solition.
+
+
+--for all of them--install-package Entityframework
+install-package Microsoft.aspnet.Identity.core
+install-package Microsoft.aspnet.Identity.EntityFramework
+install-package Newtonsoft.Json
+install-package Microsoft.aspnet.Identity.Owin
+--sadece UI--
+install-package Microsoft.Owin
+install-package Microsoft.Owin.Security
+install-package Microsoft.Owin.Security.Cookies
+install-package Microsoft.Owin.Security.OAuth
+install-package Microsoft.Owin.Host.SystemWeb
+
+--Api için
+install-package Microsoft.AspNet.WebApi.Cors
+install-package Microsoft.Owin.Cors
+install-package Microsoft.AspNet.WebApi.Owin
